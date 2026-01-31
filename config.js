f0ee3078266b142e089dae6862dab07a3b7da749
sfeng <mangokny@gmail.com>
2026-01-31 17:17:20 -0500

    Update config.js
    used cloudinary for song url

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://drive.google.com/file/d/1v7oYiAbrcljC48FzZcMT-hy3xDSz2ZZC/view?usp=sharing", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dkmsuk4jz/video/upload/v1769897756/hao_xiang_ni_speed_up_gdvltg.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
